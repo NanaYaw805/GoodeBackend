d546a9af4ef3ef3546d37f3a97e27a5c4b193f01
NanaYaw805 <arhinyaw64@gmail.com>
2025-01-06 17:28:51 +0000

    initial commit

---
+++ goode/src/main/java/com/symergy/goode/GoodeApplication.java
@@ -0,0 +1,13 @@
+package com.symergy.goode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodeApplication.class, args);
+	}
+
+}
---
+++ goode/src/test/java/com/symergy/goode/GoodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symergy.goode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
